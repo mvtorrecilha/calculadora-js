ce2202aeea7c491921c44c4e607186def3d1797e
Marcos Vinícius <marcos.torrecilha@gmail.com>
2019-11-20 08:40:37 -0300

    Implementação da função de multiplicação

---
+++ spec/calculadora/multiplicarSpec.js
@@ -0,0 +1,23 @@
+describe("Suite de testes de multiplicação", function () {
+	var Calculadora = require('../../src/js/calculadora.js');
+
+	it('deve retornar 6 para 2 e 3', function () {
+		expect(Calculadora.multiplicar(2,3)).toEqual(6);
+	});
+
+	it('deve retornar 6 para 2 e 3 no formato texto', function () {
+		expect(Calculadora.multiplicar('2','3')).toEqual(6);
+	});
+
+	it('deve retornar 4.5 para 1.5 e 3', function () {
+		expect(Calculadora.multiplicar(1.5,3)).toEqual(4.5);
+	});
+
+	it('deve retornar 0 quando valor 1 inválido', function () {
+		expect(Calculadora.multiplicar(undefined, 5)).toEqual(0);
+	});
+
+	it('deve retornar 0 quando valor 2 inválido', function () {
+		expect(Calculadora.multiplicar(5, undefined)).toEqual(0);
+	});
+});
